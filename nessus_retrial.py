2194f4411bb7484589cf313b50d71480de4008cb
neolead <janvarry@gmail.com>
2019-02-12 02:09:02 +0300

    Update nessus_retrial.py
    bugfix for win

--- nessus_retrial.py
+++ nessus_retrial.py
@@ -22,7 +22,7 @@ name = mailx
 
 email = mailx + '@getnada.com' 
 print ("Email Address: "+ email)
-print ("\033[1;32;10mNessus Registeration Form \033[1;32;0m")
+print ("Nessus Registeration Form")
 ht=requests.get("https://www.tenable.com/products/nessus-home", verify=False)
 bs=BeautifulSoup(ht.text,'html.parser')
 for link in bs.findAll("input",{"name":"token"}):
@@ -39,32 +39,32 @@ if r.status_code == 200:
 	keyword=bs.find("title").get_text()
 	success=keyword.split('|')
 	if str(success[0][:-1]) == 'Thank You for Registering for Nessus Home!':
-		print('\033[1;32;10m'+str(success[0][:-1])+'\033[1;32;0m')
+		print(str(success[0][:-1]))
 	elif bs.find('span',{"style":"color:#FF0000;"}).get_text():
 		os.system('clear')
-		print('\033[1;31;10m Sorry, This Email Address is already Registered for Nessus Activation Code\033[1;31;0m')
+		print('Sorry, This Email Address is already Registered for Nessus Activation Code')
 
 all = mailx + "@" + domain
-time.sleep(15)
+sleep = 15
+print ("Go sleep for:" + str(sleep) + " seconds")
+time.sleep(sleep)
 data = None
 r = requests.get(GET_INBOX + all)
 uid = (r.json()['msgs'])[0]['uid']
-print(uid)
+print("UID of email is: " + uid)
 data = None
 r = requests.get(GET_MESSAGE + uid)
 text = r.json()['html']
 regex = r"\w{4}(?:-\w{4}){4}"
 activation_code=re.search(regex,text)
-#print(text)
-print("activation code:" + activation_code.group())
-with open('nessus.txt', 'w') as f:
-     print(activation_code.group(), file=f)
+print("Activation code: " + activation_code.group())
+file = open("nessus.txt","w") 
+file.write(activation_code.group())
+file.close() 
 
 if os.name == 'nt':
-	cmdw = ('C:\\Program Files\\Tenable\\Nessus\\nessuscli fetch --register ' + str(activation_code.group()))
-	os.system(cmdw)
-	cmdw = ('C:\\Program Files\\Tenable\\Nessus\\nessuscli update')
-	os.system(cmdw)
+        os.system( '""C:\\\\Program Files\\\\Tenable\\\\Nessus\\\\nessuscli" fetch --register "' + str(activation_code.group()))
+        os.system( '""C:\\\\Program Files\\\\Tenable\\\\Nessus\\\\nessuscli" update "')
 else:
     cmd = ('nessuscli fetch --register ' + str(activation_code.group()) + ';nessuscli update; systemctl restart nessusd')
     os.system(cmd)
